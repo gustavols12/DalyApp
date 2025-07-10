dcf4eae43773ad5b7bdd109c771677e202c76cb5
gustavo <gustavols2100@gmail.com>
2025-07-09 21:57:43 -0300

    Feat: busca jogo no server components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/game";
+import Link from "next/link";
 import Image from "next/image";
+import { BsArrowRightSquare } from "react-icons/bs";
 
-export default function Home() {
+async function getDalyGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 300 } }
+    );
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching game:", error);
+  }
+}
+export default async function Home() {
+  const dalyGame: GameProps = await getDalyGame();
   return (
-    <div>
-      <h1>Subindo o projeto</h1>
-    </div>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo para você
+        </h1>
+
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full relative max-h-96 h-96">
+              <div className="z-20 absolute flex items-center justify-center gap-2 bottom-0 p-3">
+                <p className="font-bold text-white text-xl">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#fff" />
+              </div>
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-200"
+                sizes="(max-width: 768px) 100vw, (max-width:1200px) 44vw"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
+    </main>
   );
 }
